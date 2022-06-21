8021cf13ed40ea1d93fd93aaca078d4f82086b65
Lucas Santana <lucasantana78@gmail.com>
2022-06-21 14:37:29 -0300

    Inicio do projeto, Person Controller, model e service adicionados

---
+++ RestWithASPNET/RestWithASPNET/Services/Implementations/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
